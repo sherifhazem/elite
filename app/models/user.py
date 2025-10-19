0f3991aefbb2ad2d0f30317ec790e732c2faf440
sherifhazem <71900215+sherifhazem@users.noreply.github.com>
2025-10-19 11:30:36 +0300

    updat users model

--- app/models/user.py
+++ app/models/user.py
@@ -33,7 +33,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128), nullable=False)
+    password_hash = db.Column(db.String(512), nullable=False)
     role = db.Column(
         db.String(50),
         default="member",
