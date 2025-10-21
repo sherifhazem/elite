976ea68e33d90f99a25e98a3f22a278d949a05f6
sherifhazem <71900215+sherifhazem@users.noreply.github.com>
2025-10-21 11:39:44 +0300

    fix defult value in comp stat

--- app/models/company.py
+++ app/models/company.py
@@ -26,7 +26,7 @@ class Company(db.Model):
     notification_preferences = db.Column(
         MutableDict.as_mutable(db.JSON), default=dict, nullable=False
     )
-    status = db.Column(db.String(20), default="pending", nullable=False)
+    status = db.Column(db.String(20), nullable=False, default='pending', server_default='pending')
     admin_notes = db.Column(db.Text, nullable=True)
 
     owner = db.relationship(
